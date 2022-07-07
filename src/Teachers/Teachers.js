fe8cec3c651231cdf058b235ee4644fa80902b96
Akash Ghosh <ag1229222@gmail.com>
2022-07-07 23:45:57 +0600

    a

--- src/Teachers/Teachers.js
+++ src/Teachers/Teachers.js
@@ -11,7 +11,7 @@ const Teachers = () => {
         }
         className='flex justify-center items-center px-12 '>
             <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
+                <img className=' lg:max-w-md lg:max-h-64 mb-5  rounded-full ring ring-primary ring-offset-base-100 ring-offset-2' src={teacher} alt="missing" />
             </div>
             <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
@@ -34,7 +34,7 @@ const Teachers = () => {
         }
         className='flex justify-center items-center px-12 '>
             <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
+                <img className='mt- lg:max-w-md lg:max-h-64 mb-5  rounded-full ring ring-primary ring-offset-base-100 ring-offset-2' src={teacher} alt="missing" />
             </div>
             <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
@@ -57,7 +57,7 @@ const Teachers = () => {
         }
         className='flex justify-center items-center px-12 '>
             <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
+                <img className='lg:max-w-md lg:max-h-64 mb-5  rounded-full ring ring-primary ring-offset-base-100 ring-offset-2' src={teacher} alt="missing" />
             </div>
             <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
@@ -74,213 +74,7 @@ const Teachers = () => {
             </div>
         </section>
      
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
-            <section 
-        style={
-            {background}
-        }
-        className='flex justify-center items-center px-12 '>
-            <div className='flex-1  lg:block'>
-                <img className='mt-[-50px] lg:max-w-md lg:max-h-64 mb-5' src={teacher} alt="missing" />
-            </div>
-            <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>মোঃ আসাদুজ্জামান আসাদ</h3>
-                <h2 className='text-3xl py-5'>প্রধান শিক্ষক</h2>
-                <p className=' pb-5'>
-                    
-                    
-                    ডিজিটাল বাংলাদেশ গড়া বর্তমান সরকারের ভিশন এবং এই ভিশনকে বাস্তবে রূপ দিতে আমরা সবাই একযোগে কাজ করে যাচ্ছি। এই চমৎকার অর্জনের সাথি হতে পেরে আমরা খুব খুশি। মৃজাপুর মাধ্যমিক বিদ্যালয় শিক্ষা বিস্তারের এবং ছাত্র-ছাত্রীদের উজ্জল ভবিষ্যৎ গঠনে গুরুত্বপূর্ণ ভূমিকা রাখবে বলে আমাদের বিশ্বাস।<br></br> 
-
-
-                মৃজাপুর মাধ্যমিক বিদ্যালয়ের ডায়নামিক ওয়েবসাইটটি তথ্যপ্রযুক্তির আধুনিকতম সংযোজন। শিক্ষাবিষয়ক তথ্য সর্বজনীন করার এ প্রচেষ্টা সকল শিক্ষার্থীর মেধাকে উৎকর্ষের কাছাকাছি নিয়ে যাবে বলে আশা করছি। এ স্কুলের সকল শিক্ষক ও শিক্ষার্থী প্রযুক্তির স্পর্শে বিকশিত হোক; এ শুভ কামনা রইল।
-
-            </p>
-            </div>
-        </section>
-     
+      
         
         </div>
     );
