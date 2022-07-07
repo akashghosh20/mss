e545b96c87fae5d35566b5b1cd7dacff12273af2
Akash Ghosh <ag1229222@gmail.com>
2022-07-07 11:58:34 +0600

    a

--- src/Teachers/Teachers.js
+++ src/Teachers/Teachers.js
@@ -4,20 +4,121 @@ import background from '../Assets/images/bg.png'
 
 const Teachers = () => {
     return (
-        <section 
+        <div>
+            <section 
         style={
             {background}
         }
         className='flex justify-center items-center'>
-            <div className='flex-1 hidden lg:block'>
-                <img className='mt-[-100px]' src={teacher} alt="missing" />
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
             </div>
             <div className='flex-1 px-5'>
-                <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-                <h2 className='text-3xl py-5'>Make an Appointment Today</h2>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
                 <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
             </div>
         </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+            <section 
+        style={
+            {background}
+        }
+        className='flex justify-center items-center'>
+            <div className='flex-1  lg:block'>
+                <img className='mt-[-50px] max-w-md max-h-64 mb-5' src={teacher} alt="missing" />
+            </div>
+            <div className='flex-1 px-5'>
+                <h3 className='text-xl text-primary font-bold'>Md.Assadujjaman Asad</h3>
+                <h2 className='text-3xl py-5'>Head Teacher</h2>
+                <p className=' pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+            </div>
+        </section>
+        
+        </div>
     );
 };
 
