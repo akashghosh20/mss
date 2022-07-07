61759d3d81507600d6835becdd9898a6b261bc58
Akash Ghosh <ag1229222@gmail.com>
2022-07-07 09:15:11 +0600

    a

--- src/Results/ResultsShow.js
+++ src/Results/ResultsShow.js
@@ -5,7 +5,7 @@ const ResultsShow = ({result}) => {
     return (
        <div>
 <div class="card-body text-center">
-                <h2 class="text-xl font-bold text-secondary">{result.name}</h2>
+                <h2 class="text-xl font-bold text-secondary">Results of Class Six is Here</h2>
                 <p>{result.class}</p>
                 <p>{result.newClassRoll}</p>
                 <p>{result.totalNumber}</p> 
